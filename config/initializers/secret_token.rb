411d2cc83af2c8467bb04c383a9c485e8e61fd58
Eric Paxton <heypaxton@gmail.com>
2014-03-05 21:09:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salemup::Application.config.secret_key_base = '6289bff028b6b75d08bc14bcb64572bf55166d26fcbbc06b6891863b4c7eb8f4ad875f2cb27d37ece267fa9160d17a1fc66ceabdd0cf53e7f84a35c2e82be7b1'
