ac1977f3b60304722fb5435c98718117f6d01e28
Eric Paxton <heypaxton@gmail.com>
2014-03-10 18:46:37 -0700

    Commit raw data to database

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -2,4 +2,18 @@ class Importer
   def fetch_feed(feed)
     xml = SimpleRSS.parse open(feed)  
   end
+
+  def commit_articles(data)
+    if data != nil
+      data.items.each do |item|
+        Article.create(
+          title: item.title,
+          description: item.description,
+          published_date: item.pubDate
+        )
+      end
+    else
+      nil
+    end
+  end
 end
--- spec/models/importer_spec.rb
+++ spec/models/importer_spec.rb
@@ -8,7 +8,13 @@ describe Importer do
     xml.channel.title.should_not eq nil
   end
 
-  xit "should import articles in a database" do
-
+  it "should import articles in a database" do
+    importer = Importer.new
+    feedPath = Rails.root.join('spec','fixtures') + 'articles'
+    data = importer.fetch_feed(feedPath)
+    importer.commit_articles(data)
+    raw_data_title = data.entries.first.title
+    database_data_title = Article.find_by_title(raw_data_title).title
+    database_data_title.should eq raw_data_title
   end
 end
